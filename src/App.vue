a5440ffc9a66014e6e9a43a1cf7a27cccc17e72e
unknown <a3521110093@alumno.uttehuacan.edu.mx>
2022-09-23 12:37:45 -0500

    Primera version 03-directivas 22 sep 2022 a las 12:34

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1> Directivas en VUE.js</h1>
+    <hr />
+    <h2>v-text</h2>
+    <span v-text="'Hola desde la directiva v-text'"></span>
+    <br>
+    <span>Hola desde la directiva v-text</span>
+    <br>
+    <span v-text="titulo"></span>
+    <br>
+    Esta es una transmutacion de texto con una variable de prop
+  </div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      titulo:"Hola desde VUE.js",
+    })
+  };
+</script>
+<style>
+  div {
+    text-align: center;
+    margin: 0px 2rem;
+  }
+</style>
+
