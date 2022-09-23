08e1c0ff6eca5fd9eba2306ff3c51144fc09b507
AloGit22 <a3521110093@alumno.uttehuacan.edu.mx>
2022-09-23 12:58:33 -0500

    Segunda version de 03-directivas 12:49

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>Universidad Tecnologica de Tehuacan</h1>
     <h1> Directivas en VUE.js</h1>
     <hr />
-    <h2>v-text</h2>
+    <h2>APW</h2>
     <span v-text="'Hola desde la directiva v-text'"></span>
     <br>
     <span>Hola desde la directiva v-text</span>
